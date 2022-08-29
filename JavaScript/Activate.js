48776bf5ca0bd224ef812232444bfff052ccafd0
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-08-29 07:29:26 +0530

    Arrray Starterd

---
+++ JavaScript/Activate.js
@@ -0,0 +1,18 @@
+function user()
+{
+alert("Shiyam is java full stack developer")
+}
+function user1(name)
+{
+    alert(name+" founded")
+}
+
+let shiyam1=()=>
+{
+    alert("Fat Arrow Function")
+}
+
+let shiyam=()=>
+{
+    return "Shiyam is stduing for sona college"
+}
